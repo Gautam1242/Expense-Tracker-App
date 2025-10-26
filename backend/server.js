ecb6d51241a1f351f8f45d7d5d153e6c07e2dd04
Gautam Shah <gautamshah317@gmail.com>
2025-10-26 20:41:26 +0530

    setup neon database and initialized successfully

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,37 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+import { sql } from "./config/db.js";
 
-const app=express()
+dotenv.config();
 
-app.get("/",(req,res)=>{
-    res.send("It is working fine...")
-})
+const PORT = process.env.PORT || 5001;
 
-app.listen(5001,()=>{
-    console.log("Server is running on port 5001")
-})
+async function initDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions(
+        id SERIAL PRIMARY KEY,
+        user_id VARCHAR(255) NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        amount DECIMAL(10,2) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`;
+
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Error initializing DB", error);
+    process.exit(1); // status code 1 means failure , 0 means success
+  }
+}
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("It is working fine...");
+});
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port : ${PORT}`);
+  });
+});
